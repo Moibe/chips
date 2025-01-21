9dfe79383a8fd987da843bf4476c3533a602a99c
Moibe <moi_estrello@hotmail.com>
2025-01-21 01:17:29 -0600

    Fix comments and test actualizar_quota.sh

--- sulku-quota/actualizar_quota.sh
+++ sulku-quota/actualizar_quota.sh
@@ -6,10 +6,10 @@ archivo="chips/sulku-quota/quota.txt"
 # Leer el número actual del archivo
 numero_actual=$(cat "$archivo")
 
-# Sumar 5 cada 5 minutos, es la tasa para PRO, o 1 cada 5 minutos la tasa para normal.
+# Sumar 5 cada 5 minutos, es la tasa para PRO, o 1 cada 5 minutos en la tasa normal.
 nuevo_numero=$((numero_actual + 5))
 
-# Verificar si el nuevo número es mayor a 31500 y ajustarlo si es necesario
+# Verificar si el nuevo número es mayor a 1500 y ajustarlo si es necesario
 if [ "$nuevo_numero" -gt 1500 ]; then
   nuevo_numero=1500
 fi
