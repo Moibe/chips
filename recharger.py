64522bd2ff9832dd0c5143e752c1ac087ae3fe23
Moibe <moi_estrello@hotmail.com>
2025-01-23 22:24:20 -0600

    Recharger

--- recharger.py
+++ recharger.py
@@ -2,4 +2,4 @@ import sulkusers_credits.recargaUsuario as credits
 
 #Recarga usuarios 
 
-credits.recarga_usuario("ella182", ambiente="prod", creditos=100)
+credits.recarga_usuario("sergiojimenez90", ambiente="prod", creditos=1)
