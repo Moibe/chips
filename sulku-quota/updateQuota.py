964dde43f0adabc806b1d9bf9e00ebf5d551fe9d
Moibe <moi_estrello@hotmail.com>
2025-02-16 19:43:57 -0600

    Seconds Limit

--- sulku-quota/updateQuota.py
+++ sulku-quota/updateQuota.py
@@ -4,4 +4,9 @@ quota_actual = fireWhale.obtenDato("quota", "quota", "segundos")
 print("La quota actual que hay es: ", quota_actual)
 quota_nueva = quota_actual + 5
 print("La quota nueva es: ", quota_nueva)
+
+#No se puede pasar de 1500.
+if quota_nueva >= 1500:
+    quota_nueva = 1500
+
 fireWhale.editaDato("quota", "quota", "segundos", quota_nueva)
